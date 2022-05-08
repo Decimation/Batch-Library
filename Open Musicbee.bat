d7c4e5e7ac9defd2caea857d305713b5e5f988b1
Read <decimation001@gmail.com>
2022-05-07 23:53:39 -0500

    Create Open Musicbee.bat

---
+++ Open Musicbee.bat
@@ -0,0 +1,3 @@
+@echo off
+rem pwsh -command "Import-Module Appx -UseWindowsPowerShell; Get-AppxPackage -Name '*musicbee*' | select -Property InstallLocation"
+"C:\Program Files\WindowsApps\50072StevenMayall.MusicBee_3.4.2.0_x86__kcr266et74avj\win32\MusicBee.exe" /openmainplayer
