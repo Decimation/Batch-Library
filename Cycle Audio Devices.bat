1afabf77bfe19dbddd3f5b6a18ec6575eb7e0896
Read <decimation001@gmail.com>
2022-06-02 14:29:36 -0500

    2022-06-02 14:29:36

--- Cycle Audio Devices.bat
+++ Cycle Audio Devices.bat
@@ -1,5 +1,7 @@
 :: Input: Audio devices to cycle through, delimited by a comma
 :: Uses AudioDevices PowerShell script
+:: Example (Batch): "Cycle Audio Devices.bat" "@('FiiO DAC','Speakers')"
+:: Example (PS): & '.\Cycle Audio Devices.bat' "@('FiiO DAC','Speakers')"
 
 @echo off
 setlocal EnableDelayedExpansion
@@ -8,11 +10,18 @@ setlocal EnableDelayedExpansion
 
 set script=%userprofile%\Documents\PowerShell\Scripts\AudioDevices.ps1
 
-if exist %script% (
-    echo "%script%"
+if not exist %script% (
+    rem echo "%script%"
+    echo Missing script
+	exit /b -1
+
 )
 
+rem echo %1
+rem echo %2
+
 set "option="
+
 for %%a in (%*) do (
    if not defined option (
       set arg=%%a
@@ -26,14 +35,17 @@ for %%a in (%*) do (
 )
 
 if not defined option (
-	set option="@('FiiO DAC','Speakers','Headset')"
+	rem set option="@('FiiO DAC','Speakers','Headset')"
 	rem set option="@(%DefaultAudioDevices%)"
-
+	echo Missing arguments
+    exit /b -1
 )
 
 SET option
 
 echo "Input: %option%"
-echo "Default input: %DefaultAudioDevices%"
+if exist %DefaultAudioDevices% (
+    echo "Default input: %DefaultAudioDevices%"
+)
 
 pwsh.exe -c " . %script% -Names %option% -MultimediaDefault"
