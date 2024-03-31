84ef78e81152f1daa88d3d7b8f43db946b9bcea0
Read <decimation001@gmail.com>
2024-03-30 22:17:55 -0500

    ​

--- Musicbee stop.bat
+++ Musicbee stop.bat
@@ -1,4 +1,4 @@
 @echo off
 
 rem pwsh -command "Import-Module Appx -UseWindowsPowerShell; Get-AppxPackage -Name '*musicbee*' | select -Property InstallLocation"
-"C:\Program Files\WindowsApps\50072StevenMayall.MusicBee_3.4.2.0_x86__kcr266et74avj\win32\MusicBee.exe" /stop
+"C:\Program Files (x86)\MusicBee\MusicBee.exe" /stop
---
+++ VolumeSwitch.bat
@@ -0,0 +1,4 @@
+@echo off
+set script=%userprofile%\Documents\PowerShell\Scripts\VolumeSwitch.ps1
+
+pwsh.exe -f "%script%"
